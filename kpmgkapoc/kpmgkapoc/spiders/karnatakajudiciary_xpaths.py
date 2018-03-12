bab3a962e59f7d7d946a5d605abdeab0d996f076
chkiranmayi <kiranmayi@headrun.net>
2018-03-12 12:10:52 +0530

    kpmgkapoc

--- kpmgkapoc/kpmgkapoc/spiders/karnatakajudiciary_xpaths.py
+++
@@ -1,8 +0,0 @@
-event_validation_xpath = '//input[@name="__EVENTVALIDATION"]/@value'
-view_state_xpath = '//input[@name="__VIEWSTATE"]/@value'
-generator_xpath = '//input[@name="__VIEWSTATEGENERATOR"]/@value'
-benches_xpath = '//select[@name="ctl00$ContentPlaceHolder1$ddlBench"]/option'
-petresdonts_xpath = 'select[@name="ctl00$ContentPlaceHolder1$ddlPetRespDont"]/option'
-districts_xpath = 'select[@name="ctl00$ContentPlaceHolder1$ddlPetRespDont"]/option'
-table_data_xpath = '//table[@id="ctl00_ContentPlaceHolder1_GrvDetails"]//tr'
-
