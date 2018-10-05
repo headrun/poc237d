db440b624b5d632dc42f5989ef98d742c0c9db09
ERamyaSekhar <ramya@headrun.com>
2018-10-05 15:40:28 +0530

    Adding Interpol Version2

--- KPMG/Interpol/Interpol/spiders/v2/interpol_latest.py
+++ KPMG/Interpol/Interpol/spiders/v2/interpol_latest.py
@@ -1,15 +1,17 @@
 ''' This script is reilated to interpol it disobeys the robots.txt
 '''
 import datetime
-import os
 import csv
 
 from scrapy.http import FormRequest, Request
 from scrapy.selector import Selector
 from scrapy.spiders import BaseSpider
 
-from Interpol.spiders.xpaths_file import *
-from Interpol.spiders.headers_file import *
+import xpaths_file 
+import headers_file 
+
+def extract_data(data, path, delem=''):
+    return delem.join(i.strip() for i in data.xpath(path).extract() if i).strip().replace('\t', '').replace('\n', '').replace(';', ',')
 
 class Interpol(BaseSpider):
     '''
@@ -22,27 +24,17 @@ class Interpol(BaseSpider):
     def __init__(self, keyword='', *args, **kwargs):
         super(Interpol, self).__init__(*args, **kwargs)
         self.keyword = keyword
-        self.filename = "interpol%s.csv" % (str(datetime.datetime.now().date()))
-        self.csv_file = self.is_path_file_name(self.filename)
-        self.fields = ["url", "Family_name", "Criminal_Name", "sex", "Date_of_birth", "Place_of_birth", "Language_spoken", "Nationality", "Charges", "Regions_where_wanted"]
+	self.fields = ["url", "Family_name", "Criminal_Name", "sex", "Date_of_birth", "Place_of_birth", "Language_spoken", "Nationality", "Charges", "Regions_where_wanted"]
+	oupf = open('Interpol-%s.csv'  % (str(datetime.date.today())), 'wb+')
+	self.csv_file  = csv.writer(oupf)
         self.csv_file.writerow(self.fields)
-
-    def is_path_file_name(self, excel_file_name):
-        '''
-        This function contains csv_file generation
-        '''
-        if os.path.isfile(excel_file_name):
-            os.system('rm%s' % excel_file_name)
-        oupf = open(excel_file_name, 'ab+')
-        todays_excel_file = csv.writer(oupf)
-        return todays_excel_file
 
     def parse(self, response):
         '''
         This function contains last pages_ navigations
         '''
         reference = response.url
-        headers = headers_list
+        headers = headers_file.headers_list
 
         data = [('search', '1'), ('Name', self.keyword), ('Forename', ''), ('Nationality', ''), ('FreeText', ''), ('current_age_mini', '0'), ('current_age_maxi', '100'), ('Sex', ''), ('Eyes', ''), ('Hair', ''), ('RequestingCountry', ''), ('data', ''),]
 
@@ -63,17 +55,18 @@ class Interpol(BaseSpider):
         '''
         This function contains required xpaths
         '''
-        sel = Selector(response)
+	sel = Selector(response)  
         reference = response.url
-        family_name = ''.join(sel.xpath(family_name_xpath).extract())
-        fore_name = ''.join(sel.xpath(fore_name_xpath).extract())
-        sex = ''.join(sel.xpath(sex_xpath).extract())
-        date_of_birth = ''.join(sel.xpath(date_of_birth_xpath).extract())
-        place_of_birth = ''.join(sel.xpath(place_of_birth_xpath).extract())
-        language_spoken = ''.join(sel.xpath(language_spoken_xpath).extract()).replace('\t', '').replace('\n', '')
-        nationality = ''.join(sel.xpath(nationality_xpath).extract()).replace('\t', '').replace('\n', '')
-        charges = ''.join(sel.xpath(charges_xpath).extract()).replace(';', ',')
-        regions_where_wanted = str(''.join(sel.xpath(regions_where_wanted_xpath).extract()))
+	family_name = extract_data(sel, xpaths_file.family_name_xpath)
+	fore_name = extract_data(sel, xpaths_file.fore_name_xpath)
+        fore_name = extract_data(sel, xpaths_file.fore_name_xpath)
+        sex = extract_data(sel, xpaths_file.sex_xpath)
+        date_of_birth = extract_data(sel, xpaths_file.date_of_birth_xpath)
+        place_of_birth = extract_data(sel, xpaths_file.place_of_birth_xpath)
+        language_spoken = extract_data(sel, xpaths_file.language_spoken_xpath)
+        nationality = extract_data(sel, xpaths_file.nationality_xpath)
+        charges = extract_data(sel, xpaths_file.charges_xpath)
+        regions_where_wanted = extract_data(sel, xpaths_file.regions_where_wanted_xpath)
         csv_values = [reference, family_name.encode('utf8'), fore_name.encode('utf8'), sex, date_of_birth, place_of_birth, language_spoken, nationality, charges.encode('utf8'), regions_where_wanted.encode('utf8')]
         self.csv_file.writerow(csv_values)
 
